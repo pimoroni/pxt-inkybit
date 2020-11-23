08234aca9bb45e65dd72d13df6d0e4c3a17a71e1
Phil Howard <phil@gadgetoid.com>
2020-11-23 12:18:52 +0000

    Variable text size, many tweaks

--- inkybit.ts
+++ inkybit.ts
@@ -1,4 +1,4 @@
-//% weight=100 color=#000000 icon="\uf06e" block="Inky:Bit"
+//% weight=100 color=#000000 icon="\uf043" block="Inky:Bit"
 namespace inkybit {
     
     const WIDTH: number = 250
@@ -60,15 +60,40 @@ namespace inkybit {
         "NorthW"
     ]
 
-    //% block
     export enum Color {
-        White = 0,
+        //% block="black" defl=True
         Black = 1,
+        //% block="white"
+        White = 0,
+        //% block="red"
         Red = 2,
+        //% block="yellow"
         Yellow = 2
     }
 
-    let pixelSize: number = 1;
+    export enum PixelSize {
+        //% block="normal (1x)" defl=True
+        Normal = 1,
+        //% block="double (2x)"
+        Double = 2,
+        //% block="triple (3x)"
+        Triple = 3,
+        //% block="quad (4x)"
+        Quad = 4
+    }
+
+    export enum TextSize {
+        //% block="regular (2x)" defl=True
+        Regular = 2,
+        //% block="tiny (1x)"
+        Tiny = 1,
+        //% block="medium (3x)"
+        Medium = 3,
+        //% block="large (4x)"
+        Huge = 4
+    }
+
+    let _pixelSize: number = 1;
 
     function tokenize(text: string): string {
         let result: string = ""
@@ -106,15 +131,15 @@ namespace inkybit {
      * @param color - color to set (0-2)
      */
     //% blockId=inkybit_set_icon
-    //% block="display icon %icon| at x %x| y %y| with color %color"
+    //% block="draw icon %icon| at x %x| y %y| with color %color| and size %size"
     //% icon.fieldEditor="gridpicker"
     //% icon.fieldOptions.width="400" icon.fieldOptions.columns="5"
     //% icon.fieldOptions.itemColour="black" icon.fieldOptions.tooltips="true"
     //% x.min=0 x.max=249
     //% y.min=0 y.max=119
-    export function setIcon(icon: IconNames, x: number, y: number, color: Color = Color.Black): void {
+    export function setIcon(icon: IconNames, x: number, y: number, color: Color = Color.Black, size: TextSize = TextSize.Regular): void {
         let image: Image = images.iconImage(icon)
-        setImage(image, x, y, color)
+        setImage(image, x, y, color, size)
     }
 
     /**
@@ -125,12 +150,12 @@ namespace inkybit {
      * @param color - color to set (0-2)
      */
     //% blockId=inkybit_set_arrow
-    //% block="display arrow %arrow| at x %x| y %y| with color %color"
+    //% block="draw arrow %arrow| at x %x| y %y| with color %color| and size %size"
     //% x.min=0 x.max=249
     //% y.min=0 y.max=119
-    export function setArrow(arrow: ArrowNames, x: number, y: number, color: Color = Color.Black): void {
+    export function setArrow(arrow: ArrowNames, x: number, y: number, color: Color = Color.Black, size: TextSize = TextSize.Regular): void {
         let image: Image = images.arrowImage(arrow)
-        setImage(image, x, y, color)
+        setImage(image, x, y, color, size)
     }
 
     /**
@@ -141,13 +166,17 @@ namespace inkybit {
      * @param color - color to set (0-2)
      */
     //% blockId=inkybit_set_image
-    //% block="display image %image| at x %x| y %y| with color %color"
+    //% block="draw image %image| at x %x| y %y| with color %color| and size %size"
     //% x.min=0 x.max=249
     //% y.min=0 y.max=119
-    export function setImage(image: Image, x: number, y: number, color: Color = Color.Black): void {
-        for (let c_row = 0; c_row < 5; c_row++) {
-            for (let c_col = 0; c_col < image.width(); c_col++) {
-                if (image.pixelBrightness(c_col, c_row)) {
+    export function setImage(image: Image, x: number, y: number, color: Color = Color.Black, size: TextSize = TextSize.Regular): void {
+        let rows: number = 5 * size;
+        let cols: number = image.width() * size;
+        for (let c_row = 0; c_row < rows; c_row++) {
+            let s_row: number = Math.floor(c_row / size);
+            for (let c_col = 0; c_col < cols; c_col++) {
+                let s_col: number = Math.floor(c_col / size);
+                if (image.pixelBrightness(s_col, s_row)) {
                     setPixel(x + c_col, y + c_row, color)
                 }
             }
@@ -161,17 +190,17 @@ namespace inkybit {
      * @param color - color to set (0-2)
      */
     //% blockId=inkybit_set_pixel
-    //% block="display image %image| at x %x| y %y| with color %color"
+    //% block="set pixel at x %x| y %y| with color %color"
     //% x.min=0 x.max=249
     //% y.min=0 y.max=119
     export function setPixel(x: number, y: number, color: Color = Color.Black): void {
-        x *= pixelSize;
-        y *= pixelSize;
+        x *= _pixelSize;
+        y *= _pixelSize;
         let c: number = color
         let px: number = 0;
         let py: number = 0;
-        for(py = 0; py < pixelSize; py++) {
-            for(px = 0; px < pixelSize; px++) {
+        for(py = 0; py < _pixelSize; py++) {
+            for(px = 0; px < _pixelSize; px++) {
                 _setPixel(x + px, y + py, c);
             }
         }
@@ -185,18 +214,19 @@ namespace inkybit {
     //% blockId=inkybit_set_pixel_size
     //% block="set pixel size to %size"
     //% advanced color=#220000
-    //% size.min=1 size.max=4
-    export function setPixelSize(size: number): void {
-        pixelSize = size;
+    //% size.defl=1
+    export function setPixelSize(size: PixelSize = PixelSize.Normal): void {
+        _pixelSize = size;
     }
 
     /**
      * Get scroll:bit pixel size
      */
     //% blockId=inkybit_get_pixel_size
+    //% block="get pixel size"
     //% advanced color=#220000
-    export function getPixelSize(): number {
-        return pixelSize;
+    export function getPixelSize(): PixelSize {
+        return _pixelSize;
     }
 
     /**
@@ -221,19 +251,24 @@ namespace inkybit {
      * @param y - y position (0-119)
      * @param color - color to set (0-2)
      */
-    export function drawChar(char: string, x: number, y: number, color: Color = Color.Black): void {
+    export function drawChar(char: string, x: number, y: number, color: Color = Color.Black, size: TextSize = TextSize.Regular): void {
+        let rows: number = 5 * size;
+        let cols: number = 5 * size;
+
         if (char.charCodeAt(0) > DAL.MICROBIT_FONT_ASCII_END + ARROWOFFSET) {
-            setArrow(char.charCodeAt(0) - DAL.MICROBIT_FONT_ASCII_END - ARROWOFFSET - 1, x, y, color)
+            setArrow(char.charCodeAt(0) - DAL.MICROBIT_FONT_ASCII_END - ARROWOFFSET - 1, x, y, color, size)
             return
         }
         if (char.charCodeAt(0) > DAL.MICROBIT_FONT_ASCII_END) {
-            setIcon(char.charCodeAt(0) - DAL.MICROBIT_FONT_ASCII_END - 1, x, y, color)
+            setIcon(char.charCodeAt(0) - DAL.MICROBIT_FONT_ASCII_END - 1, x, y, color, size)
             return
         }
         let data: Buffer = getChar(char)
-        for (let c_row = 0; c_row < 5; c_row++) {
-            for (let c_col = 0; c_col < 5; c_col++) {
-                if ((data[c_row] & (1 << (4 - c_col))) > 0) {
+        for (let c_row = 0; c_row < rows; c_row++) {
+            let s_row: number = Math.floor(c_row / size);
+            for (let c_col = 0; c_col < cols; c_col++) {
+                let s_col: number = Math.floor(c_col / size);
+                if ((data[s_row] & (1 << (4 - s_col))) > 0) {
                     setPixel(x + c_col, y + c_row, color)
                 }
             }
@@ -248,24 +283,23 @@ namespace inkybit {
      * @param brightness - brightness to set (0-255)
      */
     //% blockId=inkybit_draw_text
-    //% block="draw %text| at x %x| y %y| with color %color"
-    //% advanced color=#220000
+    //% block="draw text %text| at x %x| y %y| with color %color| and size %size"
     //% x.min=0 x.max=249
     //% y.min=0 y.max=119
-    export function drawText(text: string, x: number, y: number, color: Color = Color.Black): void {
+    export function drawText(text: string, x: number, y: number, color: Color = Color.Black, size: TextSize = TextSize.Regular): void {
         text = tokenize(text)
-        _drawText(text, x, y, color)
+        _drawText(text, x, y, color, size)
     }
-    export function _drawText(text: string, x: number, y: number, color: Color = Color.Black): void {
+    export function _drawText(text: string, x: number, y: number, color: Color = Color.Black, size: TextSize = TextSize.Regular): void {
         let o_x: number = x;
         for (let char_index: number = 0; char_index < text.length; char_index++){
-            let width:  number = charWidth(text.charAt(char_index))
-            if ((x + width) * pixelSize >= WIDTH) {
-                y += 6 // New line
+            let width:  number = charWidth(text.charAt(char_index), size)
+            if ((x + width) * _pixelSize >= WIDTH) {
+                y += 6 * size // New line, 5px tall + 1px gap
                 x = o_x
             }
-            drawChar(text.charAt(char_index), x, y, color)
-            x += width + 1 // 1px space between chars
+            drawChar(text.charAt(char_index), x, y, color, size)
+            x += width + (1 * size) // 1px space between chars
         }
     }
 
@@ -324,12 +358,12 @@ namespace inkybit {
         return getFontData(character.charCodeAt(0))
     }
 
-    function charWidth(character: string): number {
+    function charWidth(character: string, size: TextSize = TextSize.Regular): number {
         let charcode: number = character.charCodeAt(0)
         if (charcode > DAL.MICROBIT_FONT_ASCII_END) {
-            return 5
+            return 5 * size
         }
-        return getCharWidth(charcode)
+        return getCharWidth(charcode) * size
     }
 }
 
--- test.ts
+++ test.ts
@@ -9,6 +9,27 @@ for (n = 0; n < 50; n++){
     inkybit.setPixel(n, 50-n, 1)
 }
 
+inkybit.setPixelSize(inkybit.PixelSize.Normal)
+inkybit.drawText("Ahoy", 1, 1, inkybit.Color.Black, inkybit.TextSize.Regular)
+
+inkybit.drawText("Hello {Asleep}", 1, 22)
+inkybit.drawText("Test", 1, 30, inkybit.Color.Black, inkybit.TextSize.Regular)
+
+inkybit.drawText("The quick brown fox jumps over the lazy dog.", 1, 40, inkybit.Color.Black, inkybit.TextSize.Regular)
+
+//inkybit.setIcon(IconNames.Heart, 0, 1, inkybit.Color.Red, inkybit.TextSize.Tiny)
+//inkybit.drawText("Hello world this is some text", 6, 1, inkybit.Color.Black, inkybit.TextSize.Tiny)
+
+//inkybit.setIcon(IconNames.Heart, 0, 6, inkybit.Color.Red, inkybit.TextSize.Regular)
+//inkybit.drawText("Hello world this is some text", 12, 6, inkybit.Color.Black, inkybit.TextSize.Regular)
+
+//inkybit.setIcon(IconNames.Heart, 0, 41, inkybit.Color.Red, inkybit.TextSize.Medium)
+//inkybit.drawText("Hello world this is some text", 18, 41, inkybit.Color.Black, inkybit.TextSize.Medium)
+
+
+inkybit.show()
+
+/*
 
 inkybit.setPixelSize(2);
 for (n = 0; n < 50; n++){
@@ -23,14 +44,21 @@ inkybit.drawText("Hello world, this is some text with a {Tortoi}", 50, 0)
 
 inkybit.drawText("This is some long text that wraps around the right-hand edge of the display and starts at its X origin again", 200, 20);
 
+let y: number = 0;
 for (n = 1; n < 5; n++){
     inkybit.setPixelSize(n);
     inkybit.setIcon(IconNames.Heart, n * 5, 0, inkybit.Color.Red)
 
-    inkybit.drawText("Hello World", 0, n * 5);
+    inkybit.drawText("Hello World Wrap Text", 0, n * 3);
+}
+
+inkybit.setPixelSize(inkybit.PixelSize.Normal)
+for (n = 0; n < 8; n++){
+    inkybit.setArrow(n, 200 + (n * 5), 110)
 }
 
 basic.showNumber(2)
 inkybit.show()
 basic.showNumber(3)
 basic.showNumber(4)
+*/
